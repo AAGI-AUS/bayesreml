1de427102e85ff75e55585fb3ffec6f6eda1c400
Emi Tanaka <dr.emi.tanaka@gmail.com>
2025-10-08 20:59:26 +1100

    m2 model

--- scripts/ET-01-asreml.R
+++ scripts/ET-01-asreml.R
@@ -1,7 +1,10 @@
+library(tidyverse)
 library(asreml)
 dat <- agridat::barrero.maize |> 
     transform(yearf = factor(year))
 
+
+# m1 ----------------------------------------------------------------------
 m1 <- asreml(
   yield ~ loc * yearf,
   data = dat,
@@ -18,3 +21,22 @@ saveRDS(m1, here::here("outputs/01-asreml-barrero-maize.rds"))
 # pak::pak("emitanaka/broom.asreml")
 broom.asreml::tidy(m1, "fixed")
 broom.asreml::tidy(m1, "random")
+
+
+
+# m2 ----------------------------------------------------------------------
+dat <- agridat::barrero.maize |> 
+    filter(year %in% 2000:2001) |> 
+    mutate(yearf = factor(year)) |> 
+    na.omit()
+
+m2 <- asreml(
+    yield ~ env,
+    data = dat,
+    random = ~ gen + gen:env,
+    residual = ~ dsum(~ units | env),
+    workspace = "500mb"
+)
+m2$converge
+
+saveRDS(m2, here::here("outputs/01-asreml-barrero-maize-m2.rds"))
