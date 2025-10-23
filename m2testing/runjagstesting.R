e05169eb2771236e8894e0c8b636af9d4a0bf439
fhui28 <fhui28>
2025-10-23 21:50:55 +1100

    runjags testing now done on the 2000 up to 2011 datasets. Results on comp time also saved in xlsx file

--- m2testing/runjagstesting.R
+++ m2testing/runjagstesting.R
@@ -1,7 +1,5 @@
 # This is the code updated based on comments from Francis (email Friday, 19 September 2025), coding fixed effects in a different way
-# The code is written with the purpose of testing how different Bayesian modelling packages
-# (greta, INLA, brms, nimble and runjags) can handle the same (relatively complex) 
-# linear mixed model over increasing sample sizes. The initial asreml model is as follows (as per R agridat::barrero.maize):
+# The code is written with the purpose of testing how different Bayesian modelling packages (greta, INLA, brms, nimble and runjags) can handle the same (relatively complex) linear mixed model over increasing sample sizes. The initial asreml model is as follows (as per R agridat::barrero.maize):
 
 # the asreml model, see R agridat::barrero.maize
 ##################
@@ -40,80 +38,23 @@ rm(barrero.maize)
 data_initial <- data_initial %>% 
     select(-c("yor", "daystoflower", "plantheight", "earheight", "population", "lodged", "moisture", "testweight"))
 
-# # if needed, expend the initial table x times
-# ###############################
-# dataset_expend <- FALSE
-# if (dataset_expend) {
-#     
-#     x <- 100
-#     
-#     list_all_tabs <- list()
-#     list_all_tabs[[1]] <- data_initial
-#     ii0 <- 1
-#     set.seed(55)
-#     for (i0 in 2:x) {
-# 
-#         tab_new <- copy(data_initial)
-#         tab_new[, year := year + 11*ii0]
-#         tab_new[, env := paste0(yield, loc)]
-#         tab_new[, yield := sample(yield, n_initial, replace = T)]
-#         list_all_tabs[[i0]] <- tab_new
-#         ii0 <- ii0 + 1
-#     }
-#     
-#     data_initial_expended <- rbindlist(list_all_tabs)
-#     data_initial <- data_initial_expended
-#     
-#     }
-
-
-# package_name <- "jags_index"
-# 
-# dir_out <- paste0("dir_packages_tesing/", package_name, "/")
-#     data_initial_expended <- rbindlist(list_all_tabs)
-#     data_initial <- data_initial_expended
-#     
-#     }
-
-
-# package_name <- "jags_index"
-# 
-# dir_out <- paste0("dir_packages_tesing/", package_name, "/")
-# dir.create(dir_out, showWarnings = FALSE, recursive = TRUE)
-# 
-# 
-# # increase the memory limites to maximum
-# mem.maxVSize(vsize = Inf)
-# mem.maxNSize(nsize = Inf)
-# 
-# print(paste0("maximum sample size: ", nrow(data_initial)))
-
 # 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010
 # we will be taking the datasets 2000:2001, 2000:2002, ..., 2000:2010
 years_vec <- sort(data_initial$year %>% unique)
 list_computational_times <- list()
-list_draws <- list()
-ii1 <- 1
+sample_sizes_vec <- NULL
+do_summaries <- FALSE
 
-set.seed(55)
-#rand_seeds_vec <- sample(10:1000, length(years_vec), replace = FALSE)
-sample_sizes_vec <- 1:length(years_vec)*0
 
 # run through 2000:2001, 2000:2002, ..., 2000:length(years_vec)
-# for (i1 in 2:length(years_vec)) {
-i1 = 2
-#' FKCH: Using a fixed subset of years as an example to keep things simple. You can revert this back to a for loop and save results later...
-    
+for (i1 in 2:length(years_vec)) {
     years_needed <- years_vec[1:i1]
-    #seed_local <- rand_seeds_vec[i1]
-    
     print(paste0("number of years to be processed: ", length(years_needed)))
     
     # drop missing outcomes, although note jags can automatically handle this by either omitting or imputing if a distribution is assumed
     data_work <- data_initial %>%
         filter(year %in% years_needed) %>% 
         na.omit()
-    sample_sizes_vec[i1] <- nrow(data_work)
     
     # set the factors
     data_work <- data_work %>% 
@@ -123,7 +64,7 @@ i1 = 2
                loc = factor(loc),
                gen = factor(gen),
                rep = factor(rep))
-
+    
     data_work <- data_work %>% 
         mutate(loc_numeric = loc %>% factor %>% as.numeric,
                yearf_numeric = yearf %>% factor %>% as.numeric,
@@ -135,8 +76,7 @@ i1 = 2
                genloc_numeric = interaction(gen, loc) %>% factor %>% as.numeric,
                genenv_numeric = interaction(gen, env) %>% factor %>% as.numeric) 
     
-    str(data_work)
-    gc()
+    # str(data_work)
     
     ##--------------------
     # Model in asreml (benchmark)
@@ -146,10 +86,9 @@ i1 = 2
     #                      random = ~ gen + gen:env,
     #                      residual = ~ dsum( ~ units|env),
     #                      workspace="500mb")
-    
-    m2_asreml_fixed <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-fixed.rds"))
-    m2_asreml_random <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-random.rds"))
-    m2_asreml_varcomp <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-vcomp.rds"))
+    # m2_asreml_fixed <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-fixed.rds"))
+    # m2_asreml_random <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-random.rds"))
+    # m2_asreml_varcomp <- readRDS(file = here("outputs", "01-asreml-barrero-maize-m2-vcomp.rds"))
     
     
     ##--------------------
@@ -157,7 +96,7 @@ i1 = 2
     ##--------------------
     # computational time
     ptm1 <- proc.time() 
-
+    
     env_numeric <- data_work$env_numeric
     gen_numeric <- data_work$gen_numeric
     genenv_numeric <- data_work$genenv_numeric
@@ -227,11 +166,12 @@ i1 = 2
     #monitor# beta_intercept, beta_env, sigma_gen, sigma_gen_env, sigma_resid_env, u_gen, u_gen_env
     }"
     
-
+    
     ##--------------------
     ## Form model and sample...
     ##--------------------
     # Run MCMC sampling for Bayesian inference (burnin, sample, and adapt are set to defaults; these are not necessarily compatible with what brms, greta and other MCMC samples use!)
+    # Note default settings of burnin = 4000, sample = 10000, adapt = 1000 MCMC samples are used
     m2_jags <- run.jags(model = model, 
                         n.chains = 4, 
                         method = "parallel")
@@ -240,72 +180,143 @@ i1 = 2
     ##--------------------
     # Examine results -- using the posterior median and quantiles from jags as a basic check to start off with
     ##--------------------
-    s <- summary(m2_jags) #' Note this command takes a while because runjags does many of summaries. There is probably a way to produce this bad or use CODA instead, but this is a future enhancement issue
-    get_quantiles <- s %>% 
-        as.data.frame() %>% 
-        select(Lower95:Upper95) 
-    rownames(get_quantiles) <- c("Intercept",
-                                 paste0("env", levels(data_work$env)), 
-                                 "sigma_variancecom_gen",
-                                 "sigma_variancecom_gen_env",
-                                 # paste0("loc", rep(levels(data_work$loc), n_yearf), 
-                                 #        ":yearf", rep(levels(data_work$yearf), each = n_loc)),
-                                 paste0("sigma_residual_env", levels(data_work$env)),
-                                 paste0("gen_", levels(data_work$gen)),
-                                 interaction(paste0("gen_", data_work$gen), paste0("env_", data_work$env), sep = ":") %>% factor %>% levels())
+    if(do_summaries){
+        s <- summary(m2_jags) #' Note this command takes a while because runjags does many of summaries. There is probably a way to produce this bad or use CODA instead, but this is a future enhancement issue
+        get_quantiles <- s %>% 
+            as.data.frame() %>% 
+            select(Lower95:Upper95) 
+        rownames(get_quantiles) <- c("Intercept",
+                                     paste0("env", levels(data_work$env)), 
+                                     "sigma_variancecom_gen",
+                                     "sigma_variancecom_gen_env",
+                                     # paste0("loc", rep(levels(data_work$loc), n_yearf), 
+                                     #        ":yearf", rep(levels(data_work$yearf), each = n_loc)),
+                                     paste0("sigma_residual_env", levels(data_work$env)),
+                                     paste0("gen_", levels(data_work$gen)),
+                                     interaction(paste0("gen_", data_work$gen), paste0("env_", data_work$env), sep = ":") %>% factor %>% levels())
+        
+        
+        ## Fixed effects
+        data.frame(asreml = m2_asreml_fixed$estimate %>% setNames(m2_asreml_fixed$term),
+                   jags = get_quantiles[1:(1+length(levels(data_work$env))),] %>% pull("Median"))
+        
+        
+        ## Variance components
+        data.frame(asreml = m2_asreml_varcomp$estimate %>% setNames(m2_asreml_varcomp$term),
+                   jags = get_quantiles^2 %>% filter(str_detect(rownames(get_quantiles), "sigma_")) %>% pull("Median"))
+        
+        
+        ## Random effects 
+        #' FKCH: Note this is slightly more finnicky to compare as the order of the levels in the interaction differ between asreml and jags
+        data.frame(asreml = m2_asreml_random %>% 
+                       filter(estimate != 0) %>% 
+                       select(term, estimate) %>%
+                       dplyr::slice(1:length(table(data_work$gen)))
+        ) %>% 
+            mutate(jags = get_quantiles %>% 
+                       filter(str_detect(rownames(get_quantiles), "^gen_")) %>% 
+                       dplyr::slice(1:length(table(data_work$gen))) %>% 
+                       pull("Median"))
+        
+        left_join(m2_asreml_random %>% 
+                      filter(estimate != 0) %>% 
+                      select(term, estimate) %>%
+                      dplyr::slice(-(1:length(table(data_work$gen)))),
+                  get_quantiles %>% 
+                      filter(str_detect(rownames(get_quantiles), "^gen_")) %>% 
+                      dplyr::slice(-(1:length(table(data_work$gen)))) %>% 
+                      rownames_to_column(var = "term") %>%
+                      select(term, `Median`),
+                  by = "term") %>% 
+            print(n = Inf)
+        }
     
-    
-    ## Fixed effects
-    data.frame(asreml = m2_asreml_fixed$estimate %>% setNames(m2_asreml_fixed$term),
-               jags = get_quantiles[1:(1+length(levels(data_work$env))),] %>% pull("Median"))
-
-    
-    ## Variance components
-    data.frame(asreml = m2_asreml_varcomp$estimate %>% setNames(m2_asreml_varcomp$term),
-               jags = get_quantiles^2 %>% filter(str_detect(rownames(get_quantiles), "sigma_")) %>% pull("Median"))
-    
-    
-    ## Random effects 
-    #' FKCH: Note this is slightly more finnicky to compare as the order of the levels in the interaction differ between asreml and jags
-    data.frame(asreml = m2_asreml_random %>% 
-                   filter(estimate != 0) %>% 
-                   select(term, estimate) %>%
-                   dplyr::slice(1:length(table(data_work$gen)))
-               ) %>% 
-        mutate(jags = get_quantiles %>% 
-                   filter(str_detect(rownames(get_quantiles), "^gen_")) %>% 
-                   dplyr::slice(1:length(table(data_work$gen))) %>% 
-                   pull("Median"))
-    
-    left_join(m2_asreml_random %>% 
-                   filter(estimate != 0) %>% 
-                   select(term, estimate) %>%
-                   dplyr::slice(-(1:length(table(data_work$gen)))),
-              get_quantiles %>% 
-                  filter(str_detect(rownames(get_quantiles), "^gen_")) %>% 
-                  dplyr::slice(-(1:length(table(data_work$gen)))) %>% 
-                  rownames_to_column(var = "term") %>%
-                  select(term, `Median`),
-              by = "term") %>% 
-        print(n = Inf)
-    
-    
-    
+    ##--------------------
+    # Final collection of computation times
+    ##--------------------
     exec_time1 <- proc.time() - ptm1
-    list_computational_times[[ii1]] <- exec_time1
-    ii1 <- ii1+1
-    
+    sample_sizes_vec <- c(sample_sizes_vec, nrow(data_work))
+    list_computational_times[[i1-1]] <- exec_time1
+
     print(exec_time1)
-    
-    # save the results
-    file_names <- paste0(dir_out, "r_out_04a_jags_test_", i1, "_2000_",  years_vec[i1] ,".RData")
-    #save(m1_jags, exec_time1, years_vec, seed_local, file = file_names)
-    
-    rm(model_fit, m1_jags, y, u_gen_year, u_gen_loc, u_gen_env)
-    
-    
-# } # ends for (i1 in 2:s1)
+    rm(m2_jags, y, n_env, n_gen, n_genenv, N, env_numeric, gen_numeric, genenv_numeric, data_work)
+    gc()
+
+    } # ends for (i1 in 2:s1)
 
     
 
+tab_times_summary <- data.frame(sample_size = sample_sizes_vec, 
+                                times_sec = sapply(list_computational_times, function(x) x[3]))
+writexl::write_xlsx(tab_times_summary, path = "m2testing/xlsx_tab_runjags_times_m2.xlsx")
+
+
+##---------------------
+sessioninfo::session_info()
+##---------------------
+# ─ Session info ─────────────────────────────────────────────────────────────────────────────────────────────────────
+# setting  value
+# version  R version 4.3.3 (2024-02-29)
+# os       Linux Mint 22.1
+# system   x86_64, linux-gnu
+# ui       RStudio
+# language en_AU:en
+# collate  en_AU.UTF-8
+# ctype    en_AU.UTF-8
+# tz       Australia/Sydney
+# date     2025-10-23
+# rstudio  2025.05.1+513 Mariposa Orchid (desktop)
+# pandoc   3.1.3 @ /usr/bin/pandoc
+# 
+# ─ Packages ─────────────────────────────────────────────────────────────────────────────────────────────────────────
+# package     * version  date (UTC) lib source
+# agridat     * 1.24     2024-10-27 [1] CRAN (R 4.3.3)
+# bayesplot   * 1.11.1   2024-02-15 [3] CRAN (R 4.3.3)
+# cli           3.6.5    2025-04-23 [1] CRAN (R 4.3.3)
+# coda        * 0.19-4.1 2024-01-31 [3] CRAN (R 4.3.2)
+# colorspace    2.1-1    2024-07-26 [1] CRAN (R 4.3.3)
+# dplyr       * 1.1.4    2023-11-17 [1] CRAN (R 4.3.3)
+# forcats     * 1.0.0    2023-01-29 [3] CRAN (R 4.2.2)
+# generics      0.1.4    2025-05-09 [1] CRAN (R 4.3.3)
+# ggplot2     * 3.5.1    2024-04-23 [1] CRAN (R 4.3.3)
+# glue          1.8.0    2024-09-30 [1] CRAN (R 4.3.3)
+# gtable        0.3.6    2024-10-25 [1] CRAN (R 4.3.3)
+# here        * 1.0.2    2025-09-15 [1] CRAN (R 4.3.3)
+# hms           1.1.3    2023-03-21 [3] CRAN (R 4.3.1)
+# lattice       0.22-5   2023-10-24 [4] CRAN (R 4.3.3)
+# lifecycle     1.0.4    2023-11-07 [1] CRAN (R 4.3.3)
+# lubridate   * 1.9.3    2023-09-27 [3] CRAN (R 4.3.1)
+# magrittr      2.0.4    2025-09-12 [1] CRAN (R 4.3.3)
+# Matrix      * 1.6-5    2024-01-11 [4] CRAN (R 4.3.2)
+# munsell       0.5.1    2024-04-01 [1] CRAN (R 4.3.3)
+# pillar        1.11.1   2025-09-17 [1] CRAN (R 4.3.3)
+# pkgconfig     2.0.3    2019-09-22 [1] CRAN (R 4.3.3)
+# purrr       * 1.0.4    2025-02-05 [1] CRAN (R 4.3.3)
+# R6            2.6.1    2025-02-15 [1] CRAN (R 4.3.3)
+# readr       * 2.1.5    2024-01-10 [3] CRAN (R 4.3.2)
+# RhpcBLASctl   0.23-42  2023-02-11 [3] CRAN (R 4.3.2)
+# rlang         1.1.6    2025-04-11 [1] CRAN (R 4.3.3)
+# rprojroot     2.1.1    2025-08-26 [1] CRAN (R 4.3.3)
+# rstudioapi    0.17.1   2024-10-22 [1] CRAN (R 4.3.3)
+# runjags     * 2.2.2-5  2025-04-09 [1] CRAN (R 4.3.3)
+# scales        1.3.0    2023-11-28 [1] CRAN (R 4.3.3)
+# sessioninfo   1.2.2    2021-12-06 [3] CRAN (R 4.1.2)
+# stringi       1.8.4    2024-05-06 [1] CRAN (R 4.3.3)
+# stringr     * 1.5.1    2023-11-14 [1] CRAN (R 4.3.3)
+# tibble      * 3.3.0    2025-06-08 [1] CRAN (R 4.3.3)
+# tidyr       * 1.3.1    2024-01-24 [1] CRAN (R 4.3.3)
+# tidyselect    1.2.1    2024-03-11 [1] CRAN (R 4.3.3)
+# tidyverse   * 2.0.0    2023-02-22 [3] CRAN (R 4.3.1)
+# timechange    0.3.0    2024-01-18 [3] CRAN (R 4.3.2)
+# tzdb          0.4.0    2023-05-12 [3] CRAN (R 4.3.1)
+# vctrs         0.6.5    2023-12-01 [1] CRAN (R 4.3.3)
+# withr         3.0.2    2024-10-28 [1] CRAN (R 4.3.3)
+# writexl       1.5.0    2024-02-09 [3] CRAN (R 4.3.2)
+# 
+# [1] /home/fkch/R/x86_64-pc-linux-gnu-library/4.3
+# [2] /usr/local/lib/R/site-library
+# [3] /usr/lib/R/site-library
+# [4] /usr/lib/R/library
+# 
+# ────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
     
